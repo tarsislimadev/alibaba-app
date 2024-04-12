e5bf284d749bd199bc15a2c99e1552d1fe6aced5
Tarsis <br.tmvdl@gmail.com>
2024-04-12 16:57:51 -0300

    Updating app files

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,21 @@
-import  { Server } from '@brtmvdl/backend'
+import { Server } from '@brtmvdl/backend'
 
-// import  { Database } from '@brtmvdl/database'
+import { Database } from '@brtmvdl/database'
 
-import  { PORT } from './config.js'
+import { PORT } from './config.js'
 
-// const database = new Database({ type: 'fs', config: '/data' })
+const database = new Database({ type: 'fs', config: '/data' })
 
 const server = new Server()
+
+server.post('/', ({ body, datetime = Date.now() }, res) => {
+  const data = ({ datetime, ...JSON.parse(body) })
+
+  console.log(data)
+
+  database.in('access').new().writeMany(data)
+
+  return res.setJSON(data)
+})
 
 server.listen(PORT)
