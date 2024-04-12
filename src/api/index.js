c003d88164799877d9d1effbf7d095f29035f78c
Tarsis <br.tmvdl@gmail.com>
2024-04-12 16:43:58 -0300

    Updating app files

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,11 @@
-console.log('alibaba app')
+import  { Server } from '@brtmvdl/backend'
+
+// import  { Database } from '@brtmvdl/database'
+
+import  { PORT } from './config.js'
+
+// const database = new Database({ type: 'fs', config: '/data' })
+
+const server = new Server()
+
+server.listen(PORT)
